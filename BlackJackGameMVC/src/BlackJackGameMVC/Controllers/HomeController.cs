2d7e99a463a5aaf5293ff2aa0ff2c3392c0f3199
Glis <gillesv@live.be>
2016-11-06 13:43:24 +0100

    BlackJack finished BlackJack MVC finished

---
+++ BlackJackGameMVC/src/BlackJackGameMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlackJackGame.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BlackJackGameMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [JsonProperty]
+        private BlackJack _blackJack;
+
+        public IActionResult Index()
+        {
+            _blackJack = new BlackJack();
+            SetSession();
+            return View(_blackJack);
+        }
+        public IActionResult NextCard()
+        {
+            _blackJack = LoadSession();
+            _blackJack.GivePlayerAnotherCard();
+            SetSession();
+            return View("Index", _blackJack);
+        }
+        public IActionResult Pass()
+        {
+            _blackJack = LoadSession();
+            _blackJack.PassToDealer();
+            SetSession();
+            return View("Index", _blackJack);
+        }
+
+        private BlackJack LoadSession()
+        {
+            return JsonConvert.DeserializeObject<BlackJack>(HttpContext.Session.GetString("BlackJack"));
+        }
+
+        private void SetSession()
+        {
+            HttpContext.Session.SetString("BlackJack", JsonConvert.SerializeObject(_blackJack));
+        }
+    }
+}
---
+++ BlackJackGameMVC/src/BlackJackGameMVC/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace BlackJackGame.Models {
+    public class Card {
+
+        #region Properties
+        
+        public FaceValue FaceValue { get; private set; }
+        
+        public Suit Suit { get; private set; }
+        #endregion
+
+        #region Constructors
+        public Card(Suit suit, FaceValue faceValue) {
+            Suit = suit;
+            FaceValue = faceValue;
+        }
+        #endregion
+
+        }       
+    }
