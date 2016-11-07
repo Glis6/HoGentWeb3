ea925ec341ab58f04f0c998664f40a5d23f2c5bc
Glis <gillesv@live.be>
2016-11-07 13:34:29 +0100

    ApplicationDbContext created and configured

---
+++ SportStore/src/SportStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SportsStore;Integrated Security=True;");
+        }
+    }
+}
